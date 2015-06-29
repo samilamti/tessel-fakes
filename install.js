be3bb9826e7373860687c05ea58a781cb1048ef1
Sami Lamti <sami.lamti@gmail.com>
2015-06-29 10:00:27 +0200

    Rollback on installs
    Renaming of source folder during installation didn't turn out to work
    too well.

--- install.js
+++
@@ -1,6 +0,0 @@
-(function install() {
-	var path = require('path');
-	var fs = require('fs');
-	var root = path.resolve('node_modules/tessel-fakes');
-	fs.rename(root, 'node_modules/tessel');
-})();
--- uninstall.js
+++
@@ -1,6 +0,0 @@
-(function uninstall() {
-	var path = require('path');
-	var fs = require('fs');
-	var root = path.resolve('node_modules/tessel');
-	fs.rename(root, 'node_modules/tessel-fakes');
-})();
